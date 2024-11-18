f1b134c2a80188b437f2f105c54fd9c9556883ea
alphartglazer <phuahsiaoann@gmail.com>
2024-11-18 08:59:01 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ ocld1_start.py
@@ -0,0 +1,95 @@
+##----------------------------------------------------first segment thing
+#print("hello")
+#print("my name is ")
+#print("my hobby is ")
+
+##-----------------------------------------------------the variable thing
+#hobby="game"
+#print("i like to", hobby)
+
+##---------------------------------------------------------the name thing
+#name="steven"
+#title="king"
+#print("you shall know me as",title,name)
+
+
+##-------------------------------------------------------------math thing
+# num1 = 200
+# num2 = 300
+# answer = num1 + num2
+# print(num1,"+",num2,"=",answer)
+
+##------------------------------------------------------------input thing
+# name=input("what is your name? ")
+# command=input("what is your command? ")
+# print(name,"commands you to",command)
+
+##------------------------------------------------------typecasting thing
+# num1= int(input("what is ur first number? "))
+# num2= int(input("what is ur second number? "))
+# answer = num1+num2
+# print(answer)
+
+##-------------------------------------------------------conversion thing
+#convert to integer int()
+#convert to float   float()
+#convert to string  str() 
+
+##---------------------------------------------------------for loop thing
+
+# for i in range(3):    
+#      for j in range(3):
+#           print("hello")
+#      for k in range(3):
+#           print("bye") 
+
+##---------------------------------------loop thru all values of a string
+# name=input("what is the name? ")
+# for i in name:
+#      print("gimme a",i)
+#      print(i)
+# print("who is the best?",name)
+
+##----------------------------------------for loop to count numbers thing
+# for i in range(10):
+#     print(i)
+
+# for i in range(6,73,6):
+#     print(i)
+
+# for j in range(15,0,-1):
+#     print(j)
+
+##--------------------------------------------------------algorithm thing
+# name=input("what is ur name? ")
+# weekly=float(input("how much allowance in a week? "))
+# year=weekly*52
+# overall=year*10
+# print(name,"will have $"+str(overall),"from p1 to sec 4")
+
+##--------------------------calculate length of a persons full name thing
+# fname=input("what is ur first name? ")
+# lname=input("what is ur last name? ")
+# #calculate how long is the name
+# fnamelen=int(len(fname))
+# lnamelen=int(len(lname))
+# totallength=fnamelen+lnamelen
+# print("total length of",fname,lname,"is",totallength)
+
+##------------------------------------------random number generator thing
+# import random
+
+# randoms=[]
+# while len(randoms)<7:
+#     num=random.randint(1,47)
+#     if num not in randoms:
+#         randoms.append(num)
+#     else:
+#         num=random.randint(1,47)
+        
+# print(randoms)    
+
+##------------------------------------------------------------------------
+
+    
+   
